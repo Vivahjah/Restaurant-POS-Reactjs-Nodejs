a479316a784315a8dd003af6c27316a1269fc2ee
Mengwa Ugo <mengwa44@yahoo.com>
2025-01-28 20:05:29 +0100

    added Headers

--- frontend/src/components/shared/Headers.jsx
+++ frontend/src/components/shared/Headers.jsx
@@ -1,6 +1,6 @@
 import logo from '../../assets/logo.png'
-import { FaSearch } from 'react-icons/fa'
-export default function Headers(){
+import { FaBell, FaSearch, FaUserCircle } from 'react-icons/fa'
+export default function Headers() {
     return (
         <header className="flex justify-between items-center py-4 px-8 bg-[#1a1a1a]">
             {/* Logo */}
@@ -10,11 +10,27 @@ export default function Headers(){
             </div>
 
             {/* Search */}
-            <div className="flex items-center gap-4 bg-[#1f1f1f] rounded-[20px] px-5
+            <div className="flex items-center gap-4 bg-[#1f1f1f] rounded-[15px] px-5
             py-2 w-[500px]">
                 <FaSearch className="text-[#f5f5f5]" />
                 <input type="text" placeholder="Search for food" className="bg-[#1f1f1f] outline-none text-[#f5f5f5]" />
             </div>
+
+            {/* Logged User details */}
+
+            <div className='flex items-center gap-4'>
+                <div className='bg-[#1f1f1f] p-3 rounded-[15px] cursor-pointer'>
+                    <FaBell className="text-[#f5f5f5] text-2xl" />
+                </div>
+                <div className='flex items-center gap-3 cursor-pointer'>
+                    <FaUserCircle className="text-[#f5f5f5] text-4xl" />
+                    <div>
+                        <h1 className='text-md text-[#f5f5f5] font-medium'>Vivahjah</h1>
+                        <p className='text-xs text-[#ababab]'>Admin</p>
+                    </div>
+                </div>
+            </div>
+
         </header>
     )
 }
